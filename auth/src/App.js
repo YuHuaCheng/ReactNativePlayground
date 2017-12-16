d6c67bfeccd4df08981ecee3b7bca2051e863a56
Yu Hua Cheng <ycheng@pulsepoint.com>
2017-12-16 17:50:36 -0500

    auth wrap up

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from "./components/common";
+import { Header, Button, Spinner } from "./components/common";
 import LoginForm from './components/LoginForm';
+import {CardSection} from "./components/common/CardSection";
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyBkFJ6sl4rjxwfLAxRsnhq28RvNtkRxk8M',
@@ -14,13 +17,39 @@ class App extends Component {
             storageBucket: 'authudemy-821af.appspot.com',
             messagingSenderId: '581928296002'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            // when signing in, there's an 'user' object, when signing out however, 'user' will be undefined
+            if (user){
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        })
+    }
+
+    renderContent(){
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                    );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large"/>;
+        }
     }
 
     render() {
         return (
             <View >
                 <Header headerText='Authentication'/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
