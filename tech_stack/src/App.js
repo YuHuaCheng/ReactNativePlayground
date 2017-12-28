c240e66bfb47ed2c559058a56d9c8e48c96d845a
Yu Hua Cheng <ycheng@pulsepoint.com>
2017-12-28 17:11:52 -0500

    tech stack finished

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,11 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from  './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
+        // Provider component can only have one children component, so we need to wrap them up to one (here, View)
         <Provider store={createStore(reducers)}>
-            <View />
+            <View style={{ flex: 1 }}>
+                <Header headerText="Tech Stack"/>
+                <LibraryList/>
+            </View>
         </Provider>
     )
 };
